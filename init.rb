37812613a4db90f7a1c2d1acccb04ad98a22f1fb
MayamaTakeshi <mayamatakeshi@gmail.com>
2024-08-04 15:09:31 +0900

    Internal #102197 (Upgrade to redmine 5.1): adjustment to work with redmine 5.1.3

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ unless Tracker.included_modules.include? Basix::TrackerPatch
     Tracker.send(:include, Basix::TrackerPatch)
 end
 
+require File.expand_path('../lib/basix/basix_hook_listener', __FILE__)
+
 Redmine::Plugin.register :redmine_basix do
   name 'Basix plugin'
   author 'Brastel Co. Ltd.'
